8619fb272f6b0d955d5395134ded18392f46bc33
rohit-rambade <rohitrambade18@gmail.com>
2024-04-17 18:37:52 +0530

    added product images with info

--- src/constants/products/battery-list/e-rickshawBattery.js
+++ src/constants/products/battery-list/e-rickshawBattery.js
@@ -1,15 +1,18 @@
 import ERickShaw1 from "../../../assets/product-page/ERickshaw/E Rickshaw Batteries/image 1.png";
 import ERickShaw2 from "../../../assets/product-page/ERickshaw/E Rickshaw Batteries/image 2.png";
-
 import ERickShaw3 from "../../../assets/product-page/ERickshaw/E Rickshaw Batteries/image 3.png";
+
+import SR13000Info from "../../../assets/product-page/ERickshaw/E Rickshaw Batteries/details/SR13000_Info.png";
+import SR15000Info from "../../../assets/product-page/ERickshaw/E Rickshaw Batteries/details/SR15000_Info.png";
+import SR17000Info from "../../../assets/product-page/ERickshaw/E Rickshaw Batteries/details/SR17000_Info.png";
 
 export const eRikshawBattery = [
   {
     name: "E-Rickshaw",
     batteries: [
-      { image: ERickShaw1 },
-      { image: ERickShaw2 },
-      { image: ERickShaw3 },
+      { image: ERickShaw1, info: SR13000Info },
+      { image: ERickShaw2, info: SR15000Info },
+      { image: ERickShaw3, info: SR17000Info },
     ],
   },
 ];
